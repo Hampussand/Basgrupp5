b2dbfaa8d0b38b9fc0bab7b3c4dd694b40ca62dd
Victor Sahlin <victorsahlin@hotmail.com>
2024-10-17 13:59:20 +0200

    hej

--- Basgrupp5/Program.cs
+++ Basgrupp5/Program.cs
@@ -56,6 +56,7 @@ class Program
         {
             // TODO: Implementera metod för att visa inventarie 
             Console.WriteLine(inventory[i]);
+            Console.WriteLine("HEJ");
         }
     }
     static void Pause()
