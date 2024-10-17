49b9b02ca8ce7b11dae12f40aadeea677321b7d3
Hampus Sandström <sandstroms.hampus@gmail.com>
2024-10-17 13:33:38 +0200

    Fyllde kod

--- Basgrupp5/Program.cs
+++ Basgrupp5/Program.cs
@@ -1,12 +1,83 @@
-﻿namespace Basgrupp5
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿using System;
 
-            Console.WriteLine("Hej");
+using System.Collections.Generic;
+
+class Program
+
+{
+
+    static List<string> inventory = new List<string>();
+
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Välkommen till Fabrikshanteringssystemet!");
+
+
+
+        while (true)
+
+        {
+
+            Console.WriteLine("\nVälj en åtgärd:");
+
+            Console.WriteLine("1. Lägg till produkt");
+
+            Console.WriteLine("2. Visa inventarie");
+
+            Console.WriteLine("3. Avsluta");
+
+
+
+            string val = Console.ReadLine();
+
+            switch (val)
+
+            {
+
+                case "1":
+
+                    LäggTillProdukt();
+
+                    break;
+
+                case "2":
+
+                    VisaInventarie();
+
+                    break;
+
+                case "3":
+
+                    return;
+
+                default:
+
+                    Console.WriteLine("Ogiltigt val. Försök igen.");
+
+                    break;
+
+            }
+
         }
+
     }
+
+    static void LäggTillProdukt()
+
+    {
+
+        // TODO: Implementera metod för att lägga till produkt 
+
+    }
+
+    static void VisaInventarie()
+
+    {
+
+        // TODO: Implementera metod för att visa inventarie 
+
+    }
+
 }
